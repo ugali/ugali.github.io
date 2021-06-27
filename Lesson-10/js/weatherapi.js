65df5512125a770f042a69490413178703dbb020
WDD230-BYUI <udaygali07@gmail.com>
2021-06-27 05:18:49 +0300

    api url adjusted

--- Lesson-10/js/weatherapi.js
+++ Lesson-10/js/weatherapi.js
@@ -1,4 +1,4 @@
-const apiURL = "api.openweathermap.org/data/2.5/weather?id=5604473&appid=1b683f6f487cd75ad1c49c3fbeeb5689";
+const apiURL = "https:/api.openweathermap.org/data/2.5/weather?id=5604473&appid=1b683f6f487cd75ad1c49c3fbeeb5689";
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
